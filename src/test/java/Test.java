613df6db3ee1e67806c37154495d1d5aabddcab2
Lystx <julian.heeg2004@gmail.com>
2021-09-08 23:23:01 +0200

    Added README

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,6 +1,8 @@
 
+import eu.simplejson.elements.JsonString;
 import eu.simplejson.elements.object.JsonObject;
 import eu.simplejson.enums.JsonFormat;
+import eu.simplejson.helper.Json;
 
 import java.io.File;
 import java.util.UUID;
@@ -16,11 +18,7 @@ public class Test {
 
         long start = System.currentTimeMillis();
 
-        JsonObject jsonObject = new JsonObject();
-
-
-
-        jsonObject.save(new File("test.json"), JsonFormat.FORMATTED);
+        System.out.println(Json.getInstance().fromJson(new JsonString(UUID.randomUUID().toString()), UUID.class));
 
         System.out.println((System.currentTimeMillis() - start) + "ms took the operation #1!");
 
